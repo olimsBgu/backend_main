34399697393ddb100585252391b8cdc09451db45
miht-sem <miht.sem@yandex.com>
2025-01-05 20:57:45 +0200

    Added tests.yaml

---
+++ rimonim/settings/test.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'test_user',
+        'PASSWORD': 'test_password',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
