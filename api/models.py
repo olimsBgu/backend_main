e3e294b605013c11f01e194480ebba4124d5cc43
miht-sem <miht.sem@yandex.com>
2025-01-06 23:28:51 +0200

    Updated models.py and base.py

--- api/models.py
+++ api/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
     phone = models.CharField(max_length=20, blank=True, null=True)
-    personal_id = models.CharField(max_length=50, unique=True)
+    personal_id = models.CharField(max_length=50)
     user_type = models.CharField(max_length=50, choices=[('repatriate', 'Repatriate'), ('mentor', 'Mentor')])
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
     approved = models.BooleanField(default=False)
--- rimonim/settings/base.py
+++ rimonim/settings/base.py
@@ -142,12 +142,17 @@ REST_FRAMEWORK = {
     ],
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+if DEBUG:
+    # Local development email settings
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    # Production email settings
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+    EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 AUTH_USER_MODEL = "api.User"
 
