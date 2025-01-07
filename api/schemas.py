f2b7febc511a05e374fe861ab30fc510c8833fce
Igor <Igor@DESKTOP-8CFF76G>
2025-01-07 18:27:13 +0200

    data validator - email - fix

--- api/schemas.py
+++ api/schemas.py
@@ -7,16 +7,9 @@ from ninja.errors import ValidationError
 from pydantic import EmailStr, constr, validator
 
 import re
-import email_validator
 
 class VerifyEmailSchema(Schema):
-    email: str
-
-    @validator('email')
-    def validate_email(cls, value):
-        if email_validator.validate_email(value):
-            return value
-        raise ValueError('Почта не подтверждена')
+    email: EmailStr
 
 
 
