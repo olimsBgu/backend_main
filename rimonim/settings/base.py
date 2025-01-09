3c87fee3541aacf5406d90c20087d16f0cb0dc89
miht-sem <miht.sem@yandex.com>
2025-01-09 20:01:39 +0200

    Updated deploy.yml to include email env

--- rimonim/settings/base.py
+++ rimonim/settings/base.py
@@ -147,7 +147,7 @@ if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
     # Production email settings
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
     EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
     EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
