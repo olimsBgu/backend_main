ff7638601023260ff251dddcc154419c5bdc98a6
miht-sem <miht.sem@yandex.com>
2025-02-12 17:55:31 +0200

    Updated deploy yaml and settings to include CSRF_TRUSTED_ORIGINS

--- rimonim/settings/base.py
+++ rimonim/settings/base.py
@@ -27,6 +27,8 @@ DEBUG = os.getenv("DEBUG", "1") == "1"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
 # Application definition
 
 INSTALLED_APPS = [
