17db3beba4876713c8a8ce3ed9416f849ab8741a
Igor <Igor@DESKTOP-8CFF76G>
2025-03-09 18:39:14 +0200

    review fix 2

--- rimonim/settings/base.py
+++ rimonim/settings/base.py
@@ -136,23 +136,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# # URL to use when referring to static files (e.g., in templates)
-# STATIC_URL = '/static/'
-#
-# # The absolute path to the directory where static files will be collected to
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#
-# # Additional directories where Django will look for static files (useful for development)
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-if not DEBUG:
-    STATIC_URL = "/static/"
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-    STATIC_ROOT = None
-else:
+if DEBUG:
     STATIC_URL = "/static/"
     STATICFILES_DIRS = []
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+else:
+    # URL to use when referring to static files (e.g., in templates)
+    STATIC_URL = "/static/"
+    # Additional directories where Django will look for static files (useful for development)
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+    # The absolute path to the directory where static files will be collected to
+    STATIC_ROOT = None
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
